573213fad7b7830ea63e213ca04242f14e73f2e1
carsons11 <carsons11@nycstudents.net>
2025-09-17 09:26:18 -0400

    Update test.py

--- test.py
+++ test.py
@@ -14,7 +14,48 @@ z = y[0]
 print(y)
 print(z) """
 
-x = "What is your favorite food?"
+""" x = "Hello, there"
 y = x.split()
-z= y[0,1,2,3,4]
-print(z)
+z= y[0]
+print(y)
+print(z) """
+""" 
+friend_comes = True
+money = False
+  """
+""" def and_movies(friend,money):
+    if friend and money:
+        print("Going to the movies")
+    else:
+        print("I have no friends or I have no money")
+and_movies(friend_comes, money)
+ """
+
+""" def or_movies(friend,money):
+    if friend or money:
+        print("Going to the movies")
+    else:
+        print("I have no friends or I have no money")
+or_movies(friend_comes, money) """
+
+""" homework = True
+def not_movies(homework):
+    if not homework:
+        print("movie time")
+    else: 
+        print("homework time, I hate russian")
+not_movies(homework) """
+
+
+
+""" def factor(x,y):
+    if x % y == 0:
+        print (f"{y} is a factor of {x}") 
+factor(25,5) """
+
+
+def oddoreven(x,y):
+    if x % y == 0:
+        print(f"{x} is even")
+    else:
+        print(f"{x} is not even")
